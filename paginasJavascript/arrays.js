485c40b5e288a3ac0a04fce3fd0bc71ee7a596ff
Jeyson vieira <98441884+jeysonvieira@users.noreply.github.com>
2022-07-09 17:29:33 -0300

    Initial commit

---
+++ paginasJavascript/arrays.js
@@ -0,0 +1,52 @@
+const usuariosCaixa = [
+    {id: 01,
+     nome: 'Jeyson Saraiva Vieira',
+     agencia: 1234,
+     tipoconta: 001,
+     conta: 123456,
+     dv: 1,
+     saldo: 1350.89,
+     ddd: '86',
+     numero: '95897185'},
+
+
+
+
+    {id:02,
+     nome: 'Mariane Sousa Neto',
+     agencia: 4321,
+     tipoconta: 013,
+     conta: 654321,
+     dv: 2,
+     saldo: 850.44,
+     ddd: '86',
+     numero: '99982268'},
+
+
+
+
+    {id:03,
+     nome: 'Carlos Nunes Caetano',
+     agencia: 1243,
+     tipoconta: 003,
+     conta: 123654,
+     dv: 3,
+     saldo: 4550.39,
+     ddd: '86',
+     numero: '95151823'},
+
+
+
+
+    {nome: 'Charles Costa Oliveira',},
+
+
+
+    {nome: 'Isadora Carvalho Teles',},
+
+
+
+
+    {nome: 'Karynne Santos Silveira',}
+]
+
